83d9058d63a99547a280fa4d5c504eef86bfcea4
我菜得睡不着觉 <jouskaaaa@gmail.com>
2022-10-11 01:19:41 +0200

    .

---
+++ app.py
@@ -0,0 +1,38 @@
+import pymysql
+from flask import Flask
+from flask_cors import CORS
+import config
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+app.config['JSON_AS_ASCII'] = False
+CORS(app)
+
+
+def connect():
+    connection = pymysql.connect(host=config.host,
+                                 user=config.user,
+                                 password=config.password,
+                                 database=config.database)
+    return connection
+
+
+def build_dict(description, content):
+    return dict(zip([x[0] for x in description], [x for x in content]))
+
+
+@app.route("/show_all")
+def show_all():
+    conn = connect()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM MATCH_INFO')
+    record_list = list(map(lambda content: build_dict(cursor.description, content), cursor.fetchall()))
+    conn.close()
+    return record_list
+
+
+if __name__ == "__main__":
+    table = show_all();
+    for i in table:
+        print(i);
+
